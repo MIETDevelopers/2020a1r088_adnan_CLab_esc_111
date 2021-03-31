ea3012252c3ce4905a5aa95c71ee8857ee6a6573
adnansofi66 <81609661+adnansofi66@users.noreply.github.com>
2021-03-31 23:12:22 +0530

    Update Lab05.c

--- Lab05.c
+++ Lab05.c
@@ -26,29 +26,20 @@ int main()
 	int choice;
 	char repeat;
 
-	do
-	{
-		printf("Enter 1 for circle, 2 for square and 3 for rectangle:\n");
-		scanf("%d", &choice);
-		switch(choice)
-		{
-			case 1:
 				printf("Enter radius:\n");
 				scanf("%f", &radius);
 				area = areaCircle(radius);
 				circumference = 2 * pi * radius;
 				printf("area of circle = %f",area);
-				printf(" , circumference of circle = %f",circumference);
-				break;
-			case 2:
+				printf(" , circumference of circle = %f\n",circumference);
+			
 				printf("Enter side:\n");
 				scanf("%f", &side);
 				area = areaSquare(side);
 				perimeter = 4 * side;
 				printf("area of square = %f",area);
-				printf(" , perimeter of square = %f",perimeter);
-				break;
-			case 3:
+				printf(" , perimeter of square = %f\n",perimeter);
+				
 				printf("Enter length:\n");
 				scanf("%f", &length);
 				printf("Enter width:\n");
@@ -57,14 +48,10 @@ int main()
 				perimeter2 = 2 * length * width  ;
 				printf("area of rectangle = %f",area);
 				printf(" , perimeter of the rectangle = %f",perimeter2);
-				break;
-			default:
-				printf("invalid choice\n");
-				break;
+			
+			
 
-		}
-		printf("Do you want to continue (Y/N):\n");
-		scanf(" %c",&repeat);
-	}while(repeat == 'Y' || repeat == 'y');
+
 	return 0;
-}
+} 
+
